9f798c21407e218b4773fce3c525b8aeeafb3675
Myrddin27 <lsnelson33@gmail.com>
2019-01-16 13:30:11 -0600

    [mui][core] Add enumeratum JsWriter implicit

--- core/src/main/scala/com/kemesoft/scalajs/mui/core/JsWriterImplicits.scala
+++ core/src/main/scala/com/kemesoft/scalajs/mui/core/JsWriterImplicits.scala
@@ -1,10 +1,13 @@
 package com.kemesoft.scalajs.mui.core
 
 import com.payalabs.scalajs.react.bridge.JsWriter
+import enumeratum.EnumEntry
 import japgolly.scalajs.react.vdom.VdomNode
 
 import scala.scalajs.js
 
 object JsWriterImplicits {
     implicit def vdomNodeWriter: JsWriter[VdomNode] = JsWriter(_.rawNode.asInstanceOf[js.Dynamic])
+
+    implicit def enumeratumWriter[A <: EnumEntry]: JsWriter[A] = JsWriter(_.entryName)
 }
