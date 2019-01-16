ef46b151558e20405568659c4d58482f09e39ff7
Myrddin27 <lsnelson33@gmail.com>
2019-01-16 13:31:09 -0600

    [mui][core] MuiGrid - Initial impl

--- core/src/main/scala/com/kemesoft/scalajs/mui/core/MuiGrid.scala
+++ core/src/main/scala/com/kemesoft/scalajs/mui/core/MuiGrid.scala
@@ -1,6 +1,7 @@
 package com.kemesoft.scalajs.mui.core
 
 import com.payalabs.scalajs.react.bridge.{ReactBridgeComponent, WithProps}
+import enumeratum.{EnumEntry, Enum}
 
 import scala.scalajs.js
 import scala.scalajs.js.annotation.JSImport
@@ -11,27 +12,88 @@ import scala.scalajs.js.|
   */
 object MuiGrid extends ReactBridgeComponent {
 
+    import JsWriterImplicits.enumeratumWriter
+
     override protected lazy val componentValue: js.Function = RawComponent
 
     @JSImport("@material-ui/core/Grid", JSImport.Default)
     @js.native
     object RawComponent extends js.Function
 
-    def apply(alignContent: Option[String] = Some("stretch"),
-              alignItems: Option[String] = Some("stretch"),
+    def apply(alignContent: Option[GridContentAlignment] = Some(GridContentAlignment.Stretch),
+              alignItems: Option[GridItemsAlignment] = Some(GridItemsAlignment.Stretch),
               classes: Option[js.Object] = Some(js.Object()),
               className: Option[String] = None,
               component: Option[String | js.Function] = Some("div"),
               container: Option[Boolean] = Some(false),
-              direction: Option[String] = Some("row"),
+              direction: Option[GridDirection] = Some(GridDirection.Row),
               item: Option[Boolean] = Some(false),
-              justify: Option[String] = Some("flex-start"),
-              lg: Option[Int] = None,
-              md: Option[Int] = None,
-              sm: Option[Int] = None,
+              justify: Option[GridJustification] = Some(GridJustification.FlexStart),
+              xs: Option[String | Boolean | Int] = None,
+              sm: Option[String | Boolean | Int] = None,
+              md: Option[String | Boolean | Int] = None,
+              lg: Option[String | Boolean | Int] = None,
+              xl: Option[String | Boolean | Int] = None,
               spacing: Option[Int] = Some(0),
-              wrap: Option[String] = Some("wrap"),
-              xl: Option[Int] = None,
-              xs: Option[Int] = None,
+              wrap: Option[GridWrap] = Some(GridWrap.Wrap),
               zeroMinWidth: Option[Boolean] = Some(false)): WithProps = auto
+}
+
+sealed abstract class GridItemsAlignment(override val entryName: String) extends EnumEntry
+
+object GridItemsAlignment extends Enum[GridItemsAlignment] {
+    val values = findValues
+
+    case object FlexStart extends GridItemsAlignment("flex-start")
+    case object Center extends GridItemsAlignment("center")
+    case object FlexEnd extends GridItemsAlignment("flex-end")
+    case object Stretch extends GridItemsAlignment("stretch")
+    case object Baseline extends GridItemsAlignment("baseline")
+}
+
+sealed abstract class GridContentAlignment(override val entryName: String) extends EnumEntry
+
+object GridContentAlignment extends Enum[GridContentAlignment] {
+    val values = findValues
+
+    case object Stretch extends GridContentAlignment("stretch")
+    case object FlexStart extends GridContentAlignment("flex-start")
+    case object Center extends GridContentAlignment("center")
+    case object FlexEnd extends GridContentAlignment("flex-end")
+    case object SpaceBetween extends GridContentAlignment("space-between")
+    case object SpaceAround extends GridContentAlignment("space-around")
+}
+
+sealed abstract class GridDirection(override val entryName: String) extends EnumEntry
+
+object GridDirection extends Enum[GridDirection] {
+    val values = findValues
+
+    case object Row extends GridDirection("row")
+    case object RowReverse extends GridDirection("row-reverse")
+    case object Column extends GridDirection("column-reverse")
+    case object ColumnReverse extends GridDirection("column-reverse")
+}
+
+sealed abstract class GridJustification(override val entryName: String) extends EnumEntry
+
+object GridJustification extends Enum[GridJustification] {
+    val values = findValues
+
+    case object FlexStart extends GridJustification("flex-start")
+    case object Center extends GridJustification("center")
+    case object FlexEnd extends GridJustification("flex-end")
+    case object SpaceBetween extends GridJustification("space-between")
+    case object SpaceAround extends GridJustification("space-around")
+    case object SpaceEvenly extends GridJustification("space-evenly")
+}
+
+sealed abstract class GridWrap(override val entryName: String) extends EnumEntry
+
+object GridWrap extends Enum[GridWrap] {
+    val values = findValues
+
+    case object NoWrap extends GridWrap("nowrap")
+    case object Wrap extends GridWrap("wrap")
+    case object WrapReverse extends GridWrap("wrap-reverse")
 }
